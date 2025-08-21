42f0c4117eab7e9dedc811dd240d4d508dd90afb
Denis Hirn <denis.hirn@uni-tuebingen.de>
2025-08-21 09:18:31 +0200

    Fix package.json and publishing again

--- vite.config.ts
+++ vite.config.ts
@@ -7,35 +7,33 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 
 const build = process.env.LIB
   ? {
-      lib: {
-        entry: path.resolve(__dirname, "src/components/index.ts"),
-        name: "duckdb-explain-visualizer",
-        fileName: "duckdb-explain-visualizer",
-      },
-      rollupOptions: {
-        external: ["vue"],
-        output: {
-          // Provide global variables to use in the UMD build
-          // Add external deps here
-          globals: {
-            vue: "Vue",
-          },
+    lib: {
+      entry: path.resolve(__dirname, "src/components/index.ts"),
+      name: "duckdb-explain-visualizer",
+      fileName: (format) => `duckdb-explain-visualizer.${format === 'es' ? 'mjs' : 'umd.js'}`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
         },
       },
-    }
+    },
+  }
   : {
-      outDir: "dist-app",
-      target: "esnext",
-      assetsInlineLimit: 100000000,
-      chunkSizeWarningLimit: 100000000,
-      cssCodeSplit: false,
-      brotliSize: false,
-      rollupOptions: {
-        output: {
-          inlineDynamicImports: true,
-        },
+    outDir: "dist-app",
+    target: "esnext",
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
       },
-    }
+    },
+  }
 
 // https://vitejs.dev/config/
 export default defineConfig({
